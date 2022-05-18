e8fea38947267c2b4bdbc8550aa7a571754eea06
itstabishali <105268002+itstabishali@users.noreply.github.com>
2022-05-18 18:55:06 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById("mbar").addEventListener("click", ()=>{
+    document.getElementById("slm").classList.add("show-menu")
+})
+document.getElementById("close").addEventListener("click", ()=>{
+    document.getElementById("slm").classList.remove("show-menu")
+})
